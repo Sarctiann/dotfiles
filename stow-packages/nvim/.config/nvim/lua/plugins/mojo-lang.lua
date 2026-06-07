bbb4b68592109f73772a87f0e10fd117276baa82
Sarctiann <sebas.sarc@gmail.com>
2026-06-07 02:14:44 -0300

    updated mojo conf

--- stow-packages/nvim/.config/nvim/lua/plugins/mojo-lang.lua
+++ stow-packages/nvim/.config/nvim/lua/plugins/mojo-lang.lua
@@ -2,18 +2,20 @@ local DOCS_DIR = os.getenv("DOCS_DIR")
 local plugin_dir = DOCS_DIR and (DOCS_DIR .. "/SARCTIANN/LuaCode/custom_plugins/mojo.nvim/") or nil
 
 return {
-  --- @module 'mojo'
+  --- @module "mojo"
   {
     "Sarctiann/mojo.nvim",
     dev = true,
     dir = plugin_dir,
     main = "mojo",
+    --- @type Mojo-lang.Config
     opts = {
       filetype = { enabled = true },
       treesitter = { enabled = true },
       lsp = { enabled = true },
       format = { enabled = true },
       terminal = { enabled = true, auto_activate = true },
+      completion = { enabled = true },
     },
   },
 }
